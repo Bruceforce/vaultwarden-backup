54e734beb06130d9c6537212ebdddfbf6f2a5cbc
SoarinFerret <thecodye@kanto.cloud>
2019-12-24 09:06:04 +0000

    Add functionality to delete old backups

--- backup.sh
+++ backup.sh
@@ -7,13 +7,20 @@ fi
 
 if [ $TIMESTAMP = true ]
 then
-  BACKUP_FILE="$(echo "$BACKUP_FILE")_$(date "+%F-%H%M%S")"
+  FINAL_BACKUP_FILE="$(echo "$BACKUP_FILE")_$(date "+%F-%H%M%S")"
+else
+  FINAL_BACKUP_FILE=$BACKUP_FILE
 fi
 
-/usr/bin/sqlite3 $DB_FILE ".backup $BACKUP_FILE"
+/usr/bin/sqlite3 $DB_FILE ".backup $FINAL_BACKUP_FILE"
 if [ $? -eq 0 ] 
 then 
   echo "$(date "+%F %T") - Backup successfull"
 else
   echo "$(date "+%F %T") - Backup unsuccessfull"
+fi
+
+if [ ! -z $DELETE_AFTER ] && [ $DELETE_AFTER -gt 0 ]
+then
+  find $(dirname "$BACKUP_FILE") -name "$(basename "$BACKUP_FILE")*" -type f -mtime +$DELETE_AFTER -exec rm -f {} \; -exec echo "Deleted {} after $DELETE_AFTER days" \;
 fi
