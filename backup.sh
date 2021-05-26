ce1d65ef022c4964cece65f51b59c7ed33fc3773
jmqm <jmqm@outlook.com>
2021-05-25 19:24:03 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,17 +11,4 @@ BACKUP_ATTACHMENTS=attachments # directory
 BACKUP_SENDS=sends # directory
 
 # Create a zip of the files and directories.
-#zip -r $BACKUP_ZIP $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS
 cd /data && zip -r $BACKUP_ZIP $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS && cd ..
-
-# Copy files and directories (db, rsa_key, config, attachments and sends).
-#cp /data/db.sqlite3 $BACKUP_DIRECTORY
-#cp /data/rsa_key* $BACKUP_DIRECTORY
-#cp /data/config.json $BACKUP_DIRECTORY 2>/dev/null || :
-#cp -R /data/attachments $BACKUP_DIRECTORY 2>/dev/null || :
-#cp -R /data/sends $BACKUP_DIRECTORY 2>/dev/null || :
-
-#if [ ! -z $DELETE_AFTER ] && [ $DELETE_AFTER -gt 0 ]
-#then
-#  find $(dirname "$BACKUP_FILE") -name "$(basename "$BACKUP_FILE")*" -type f -mtime +$DELETE_AFTER -exec rm -f {} \; -exec echo "Deleted {} after $DELETE_AFTER days" \;
-#fi
