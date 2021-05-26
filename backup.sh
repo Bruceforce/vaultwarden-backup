9137402e448318b5effa32f2cf865c6d2a54b827
jmqm <jmqm@outlook.com>
2021-05-25 19:17:48 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@
 BACKUP_ZIP=/backups/$(date "+%F_%H.%M.%S").zip
 
 # Create variables for the files and directories to be zipped.
-BACKUP_DB=/data/db.sqlite3
-BACKUP_RSA=/data/rsa_key*
-BACKUP_CONFIG=/data/config.json
-BACKUP_ATTACHMENTS=/data/attachments
-BACKUP_SENDS=/data/sends
+BACKUP_DB=db.sqlite3 # file
+BACKUP_RSA=rsa_key* # files
+BACKUP_CONFIG=config.json # file
+BACKUP_ATTACHMENTS=attachments # directory
+BACKUP_SENDS=sends # directory
 
 # Create a zip of the files and directories.
 #zip -r $BACKUP_ZIP $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS
