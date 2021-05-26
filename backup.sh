70629804ac0ad7b47aa6d41e4639104fd611da8b
jmqm <jmqm@outlook.com>
2021-05-25 19:10:06 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,28 +3,22 @@
 # Create variable for new backup zip.
 BACKUP_ZIP=/backups/$(date "+%F_%H.%M.%S").zip
 
-# Go inside data folder.
-cd /data
+# Create variables for the files and directories to be zipped.
+BACKUP_DB=/data/db.sqlite3
+BACKUP_RSA=/data/rsa_key*
+BACKUP_CONFIG=/data/config.json
+BACKUP_ATTACHMENTS=/data/attachments
+BACKUP_SENDS=/data/sends
 
-# Create variables for the files and folders to be zipped.
-BACKUP_DB=db.sqlite3
-BACKUP_RSA=rsa_key*
-BACKUP_CONFIG=config.json
-BACKUP_ATTACHMENTS=attachments
-BACKUP_SENDS=sends
+# Create a zip of the files and directories.
+zip -r -j $BACKUP_ZIP $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS
 
-# Create a zip of the files and folders.
-zip -r ../$BACKUP_ZIP $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS
-
-# Leave data folder.
-cd ..
-
-# Copy files and folders (db, rsa_key, config, attachments and sends).
-#cp /data/db.sqlite3 $BACKUP_FOLDER
-#cp /data/rsa_key* $BACKUP_FOLDER
-#cp /data/config.json $BACKUP_FOLDER 2>/dev/null || :
-#cp -R /data/attachments $BACKUP_FOLDER 2>/dev/null || :
-#cp -R /data/sends $BACKUP_FOLDER 2>/dev/null || :
+# Copy files and directories (db, rsa_key, config, attachments and sends).
+#cp /data/db.sqlite3 $BACKUP_DIRECTORY
+#cp /data/rsa_key* $BACKUP_DIRECTORY
+#cp /data/config.json $BACKUP_DIRECTORY 2>/dev/null || :
+#cp -R /data/attachments $BACKUP_DIRECTORY 2>/dev/null || :
+#cp -R /data/sends $BACKUP_DIRECTORY 2>/dev/null || :
 
 #if [ ! -z $DELETE_AFTER ] && [ $DELETE_AFTER -gt 0 ]
 #then
