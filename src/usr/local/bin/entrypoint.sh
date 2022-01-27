610881056f6ed89bf6cf30b645121f9edd5ddfb5
1O <2408212-1O@users.noreply.gitlab.com>
2022-01-27 23:11:44 +0100

    fix: Fixed permission issues
    fixes #19, #20, #21

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -22,61 +22,121 @@ BACKUP_CMD="/sbin/su-exec ${UID}:${GID} /app/backup.sh"
 
 debug "Running $(basename "$0") as $(id)"
 
-# Warning for deprecated settings
-if [ -n "$BACKUP_FILE" ]; then
-  warn "\$BACKUP_FILE is deprecated and will be removed in future versions. Please use \$BACKUP_DIR instead to specify the folder of the backup."
-  if [ -z "$BACKUP_DIR" ]; then
-    BACKUP_DIR=$(dirname "$BACKUP_FILE");
-    warn "Since \$BACKUP_DIR is not set defaulting to BACKUP_DIR=$BACKUP_DIR"
+check_deprecations() {
+  # Warning for deprecated settings
+  if [ -n "$BACKUP_FILE" ]; then
+    warn "\$BACKUP_FILE is deprecated and will be removed in future versions. Please use \$BACKUP_DIR instead to specify the folder of the backup."
+    if [ -z "$BACKUP_DIR" ]; then
+      BACKUP_DIR=$(dirname "$BACKUP_FILE");
+      warn "Since \$BACKUP_DIR is not set defaulting to BACKUP_DIR=$BACKUP_DIR"
+    fi
   fi
-fi
 
-# Warning for deprecated settings
-if [ -n "$BACKUP_FILE_PERMISSIONS" ]; then
-  warn "\$BACKUP_FILE_PERMISSIONS is deprecated and will be removed in future versions. Please use \$BACKUP_DIR_PERMISSIONS instead to specify the permissions of the backup folder."
-  if [ -z "$BACKUP_DIR_PERMISSIONS" ]; then
-    BACKUP_DIR_PERMISSIONS="$BACKUP_FILE_PERMISSIONS";
-    warn "Since \$BACKUP_DIR_PERMISSIONS is not set defaulting to BACKUP_DIR_PERMISSIONS=$BACKUP_FILE_PERMISSIONS"
+  # Warning for deprecated settings
+  if [ -n "$BACKUP_FILE_PERMISSIONS" ]; then
+    warn "\$BACKUP_FILE_PERMISSIONS is deprecated and will be removed in future versions. Please use \$BACKUP_DIR_PERMISSIONS instead to specify the permissions of the backup folder."
+    if [ -z "$BACKUP_DIR_PERMISSIONS" ]; then
+      BACKUP_DIR_PERMISSIONS="$BACKUP_FILE_PERMISSIONS";
+      warn "Since \$BACKUP_DIR_PERMISSIONS is not set defaulting to BACKUP_DIR_PERMISSIONS=$BACKUP_FILE_PERMISSIONS"
+    fi
   fi
-fi
 
-# Warning for deprecated settings
-if [ -n "$DB_FILE" ]; then
-  warn "\$DB_FILE is deprecated and will be removed in future versions. Please use \$VW_DATABASE_URL instead to specify the location of the source database file."
-  if [ -z "$VW_DATABASE_URL" ]; then
-    VW_DATABASE_URL="$DB_FILE";
-    warn "Since \$VW_DATABASE_URL is not set defaulting to VW_DATABASE_URL=$DB_FILE"
+  # Warning for deprecated settings
+  if [ -n "$DB_FILE" ]; then
+    warn "\$DB_FILE is deprecated and will be removed in future versions. Please use \$VW_DATABASE_URL instead to specify the location of the source database file."
+    if [ -z "$VW_DATABASE_URL" ]; then
+      VW_DATABASE_URL="$DB_FILE";
+      warn "Since \$VW_DATABASE_URL is not set defaulting to VW_DATABASE_URL=$DB_FILE"
+    fi
   fi
-fi
 
-# Warning for deprecated settings
-if [ -n "$ATTACHMENT_DIR" ]; then
-  warn "\$ATTACHMENT_DIR is deprecated and will be removed in future versions. Please use \$VW_ATTACHMENTS_FOLDER instead to specify the location of the source attachments folder."
-  if [ -z "$VW_ATTACHMENTS_FOLDER" ]; then
-    VW_ATTACHMENTS_FOLDER="$ATTACHMENT_DIR";
-    warn "Since \$VW_ATTACHMENTS_FOLDER is not set defaulting to VW_ATTACHMENTS_FOLDER=$ATTACHMENT_DIR"
+  # Warning for deprecated settings
+  if [ -n "$ATTACHMENT_DIR" ]; then
+    warn "\$ATTACHMENT_DIR is deprecated and will be removed in future versions. Please use \$VW_ATTACHMENTS_FOLDER instead to specify the location of the source attachments folder."
+    if [ -z "$VW_ATTACHMENTS_FOLDER" ]; then
+      VW_ATTACHMENTS_FOLDER="$ATTACHMENT_DIR";
+      warn "Since \$VW_ATTACHMENTS_FOLDER is not set defaulting to VW_ATTACHMENTS_FOLDER=$ATTACHMENT_DIR"
+    fi
   fi
-fi
 
-# Warning for deprecated settings
-if [ -n "$ATTACHMENT_BACKUP_DIR" ]; then
-  warn "\$ATTACHMENT_BACKUP_DIR is deprecated and will be removed in future versions. Attachment backups are stored in the \$BACKUP_DIR."
-fi
+  # Warning for deprecated settings
+  if [ -n "$LOGFILE_DIR" ]; then
+    warn "\$LOGFILE is deprecated and will be removed in future versions. Please use \$LOGFILE_DIR instead to specify the location of the source attachments folder."
+    if [ -z "$LOGFILE_DIR" ]; then
+      LOGFILE_DIR="$(dirname "$(realpath "$LOGFILE")")";
+      warn "Since \$LOGFILE_DIR is not set defaulting to LOGFILE_DIR=$LOGFILE_DIR"
+    fi
+  fi
 
-# Warning for deprecated settings
-if [ -n "$ATTACHMENT_BACKUP_FILE" ]; then
-  warn "\$ATTACHMENT_BACKUP_FILE is deprecated and will be removed in future versions. Attachment backups are stored in the \$BACKUP_DIR."
-fi
+  # Warning for deprecated settings
+  if [ -n "$ATTACHMENT_BACKUP_DIR" ]; then
+    warn "\$ATTACHMENT_BACKUP_DIR is deprecated and will be removed in future versions. Attachment backups are stored in the \$BACKUP_DIR."
+  fi
+
+  # Warning for deprecated settings
+  if [ -n "$ATTACHMENT_BACKUP_FILE" ]; then
+    warn "\$ATTACHMENT_BACKUP_FILE is deprecated and will be removed in future versions. Attachment backups are stored in the \$BACKUP_DIR."
+  fi
+}
+
+# Permissions
+adjust_permissions() {
+  if [ "$BACKUP_DIR_PERMISSIONS" -ne -1 ]; then
+    info "Adjusting permissions for $BACKUP_DIR: Setting owner $UID:$GID and permissions $BACKUP_DIR_PERMISSIONS."
+    chown "$UID:$GID" "$BACKUP_DIR"
+    chmod -R "$BACKUP_DIR_PERMISSIONS" "$BACKUP_DIR"
+  else
+    info "\$BACKUP_DIR_PERMISSIONS set to -1. Skipping adjustment of permissions."
+  fi
+
+  if [ "$LOG_DIR_PERMISSIONS" -ne -1 ]; then
+    info "Adjusting permissions for $LOG_DIR: Setting owner $UID:$GID and permissions $LOG_DIR_PERMISSIONS."
+    chown "$UID:$GID" "$LOG_DIR"
+    chmod -R "$LOG_DIR_PERMISSIONS" "$LOG_DIR"
+  else
+    info "\$LOG_DIR_PERMISSIONS set to -1. Skipping adjustment of permissions."
+  fi
+}
 
 # Initialization
-if [ ! -d "$BACKUP_DIR" ]
-then
-  info "Creating $BACKUP_DIR."
-  install -o "$UID" -g "$GID" -m "$BACKUP_DIR_PERMISSIONS" -d "$BACKUP_DIR"
+init_folders() {
+  if [ ! -d "$BACKUP_DIR" ]
+  then
+    info "Creating $BACKUP_DIR."
+    install -o "$UID" -g "$GID" -m "$BACKUP_DIR_PERMISSIONS" -d "$BACKUP_DIR"
+  fi
+
+  if [ ! -d "$LOG_DIR" ]
+  then
+    info "Creating $LOG_DIR."
+    install -o "$UID" -g "$GID" -m "$LOG_DIR_PERMISSIONS" -d "$LOG_DIR"
+  fi
+
+  adjust_permissions
+}
+
+# Initialize cron
+init_cron() {
+  if [ "$(id -u)" -eq 0 ] && [ "$(grep -c "$BACKUP_CMD" "$CRONFILE")" -eq 0 ]; then
+    info "Initalizing..."
+    debug "Writing backup command \"$BACKUP_CMD\" to cron."
+    echo "$CRON_TIME $BACKUP_CMD >> $LOGFILE_APP 2>&1" | crontab -
+  fi
+
+  # Start crond if it's not running
+  if ! pgrep crond > /dev/null 2>&1; then
+    /usr/sbin/crond -L /app/log/cron.log
+  fi
+}
+
+# Restart script as user "app:app"
+if [ "$(id -u)" -eq 0 ]; then
+  check_deprecations
+  init_folders
+  init_cron
+  debug "Restarting $(basename "$0") as app:app"
+  exec su-exec app:app "$0" "$@"
 fi
-info "Adjusting permissions for $BACKUP_DIR: Setting owner $UID:$GID and permissions $BACKUP_DIR_PERMISSIONS."
-chown "$UID:$GID" "$BACKUP_DIR"
-chmod -R "$BACKUP_DIR_PERMISSIONS" "$BACKUP_DIR"
 
 # Just run the backup script
 if [ "$1" = "manual" ]; then
@@ -84,32 +144,13 @@ if [ "$1" = "manual" ]; then
   exit 0
 fi
 
-# Initialize cron
-if [ "$(id -u)" -eq 0 ] && [ "$(grep -c "$BACKUP_CMD" "$CRONFILE")" -eq 0 ]; then
-  info "Initalizing..."
-  debug "Writing backup command \"$BACKUP_CMD\" to cron."
-  echo "$CRON_TIME $BACKUP_CMD >> $LOGFILE 2>&1" | crontab -
-
-fi
-
-# Start crond if it's not running
-if ! pgrep crond > /dev/null 2>&1; then
-  /usr/sbin/crond -L /app/log/cron.log
-fi
-
-# Restart script as user "app:app"
-if [ "$(id -u)" -eq 0 ]; then
-  debug "Restarting $(basename "$0") as app:app"
-  exec su-exec app:app "$0" "$@"
-fi
-
-info "Log level set to $LOG_LEVEL" > "$LOGFILE"
-info "Container started" >> "$LOGFILE"
-debug "Environment Variables:\n$(env)" >> "$LOGFILE"
+info "Log level set to $LOG_LEVEL" > "$LOGFILE_APP"
+info "Container started" >> "$LOGFILE_APP"
+debug "Environment Variables:\n$(env)" >> "$LOGFILE_APP"
 
 # Include cron.log in debug mode
 if [ "$LOG_LEVEL_NUMBER" -eq 7  ]; then
-  tail -f -n +1 "$LOGFILE" /app/log/cron.log
+  tail -f -n +1 "$LOGFILE_APP" "$LOGFILE_CRON"
 fi
 
-tail -f -n +1 "$LOGFILE"
+tail -f -n +1 "$LOGFILE_APP"
